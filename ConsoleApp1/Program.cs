9dfd7f41d4f021d1c7e64bf311095876330a254f
Robert Setter <r.setter@comcast.net>
2020-11-19 02:34:06 -0600

    Add integration with inspirobot.met

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,31 +3,83 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using System.IO;
 
-namespace ConsoleApp1
+namespace WebhookMadness
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Task<string> post = PostImage(ChooseFile());
-            post.Wait();
-            using (StreamWriter sw = new StreamWriter("response.json"))
+            if (args.Length == 1)
             {
-                sw.Write(post.Result);
+
+                switch (args[0].ToLower())
+                {
+                    case "--fanart":
+                        Task<string> postArt = PostImage(ChooseFile());
+                        postArt.Wait();
+                        using (StreamWriter sw = new StreamWriter("fanart_response.json"))
+                        {
+                            sw.Write(postArt.Result);
+                        }
+                        break;
+                    case "--meme":
+                        Task<string> postMeme = PostImage();
+                        postMeme.Wait();
+                        using (StreamWriter sw = new StreamWriter("meme_response.json"))
+                        {
+                            sw.Write(postMeme.Result);
+                        }
+                        break;
+
+                }
             }
         }
 
         private static async Task<string> PostImage(FileInfo chosenFile)
         {
             HttpClient httpClient = new HttpClient();
-            MultipartFormDataContent form = new MultipartFormDataContent();
-            form.Add(new StringContent("Random fanart: " + chosenFile.Name.Substring(0, chosenFile.Name.Length - 4)), "content");
-            form.Add(new ByteArrayContent(File.ReadAllBytes(chosenFile.FullName)), "file", chosenFile.Name);
+            MultipartFormDataContent form = new MultipartFormDataContent
+            {
+                { new StringContent("Random fanart: " + chosenFile.Name.Substring(0, chosenFile.Name.Length - 4)), "content" },
+                { new ByteArrayContent(File.ReadAllBytes(chosenFile.FullName)), "file", chosenFile.Name }
+            };
             HttpResponseMessage response = await httpClient.PostAsync($"https://discordapp.com/api/webhooks/768447672758566919/{Environment.GetEnvironmentVariable("wolfbros_token", EnvironmentVariableTarget.User)}", form);
             httpClient.Dispose();
             try
             {
                 return (await response.EnsureSuccessStatusCode().Content.ReadAsStringAsync());
+            }
+            catch (HttpRequestException ex)
+            {
+                return $"{ex.Message}{Environment.NewLine}From: {ex.Source}";
+            }
+        }
+
+        private static async Task<string> PostImage()
+        {
+            HttpClient httpClient = new HttpClient();
+            MultipartFormDataContent form = new MultipartFormDataContent();
+            string imgAddr;
+            HttpResponseMessage finalPost = null;
+            try
+            {
+                HttpResponseMessage response = (await httpClient.GetAsync(@"https://inspirobot.me/api?generate=true")).EnsureSuccessStatusCode();
+                imgAddr = await response.Content.ReadAsStringAsync();
+                byte[] memeData = await httpClient.GetByteArrayAsync(imgAddr);
+                if (memeData != null)
+                {
+                    form.Add(new StringContent("InspiroBot says..."), "content");
+                    form.Add(new ByteArrayContent(memeData), "file", "tempMeme.jpg"); //Filename is requred for the request to be valid.
+                    finalPost = await httpClient.PostAsync($"https://discord.com/api/webhooks/778858705197203467/{Environment.GetEnvironmentVariable("dnd_token", EnvironmentVariableTarget.User)}", form);
+                    httpClient.Dispose();
+                }
+                else
+                {
+                    finalPost = new HttpResponseMessage(System.Net.HttpStatusCode.NoContent);
+                    finalPost.Content = new StringContent("Error loading image into byte array!");
+                }
+                
+                return (await finalPost.EnsureSuccessStatusCode().Content.ReadAsStringAsync());
             }
             catch (HttpRequestException ex)
             {
